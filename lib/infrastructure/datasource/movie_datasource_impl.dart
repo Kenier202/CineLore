2b6cbd722323e5969a935b5c477dda97a099c381
kenier <kenier202@gmail.com>
2024-07-11 10:58:04 -0500

    modify movie datasource

--- lib/infrastructure/datasource/movie_datasource_impl.dart
+++ lib/infrastructure/datasource/movie_datasource_impl.dart
@@ -12,21 +12,26 @@ class MoviedbDatasource extends MovieDatasource {
     ),
   );
 
-  @override
-  Future<List<Movie>> getNowPlaying({int page = 0}) async {
-    final response = await dio.get(
-      '/movie/now_playing',
-      queryParameters: {"page": page},
-    );
-
-    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+  List<Movie> _jsonToMovies(Map<String, dynamic> json) {
+    final movieDbResponse = MovieDbResponse.fromJson(json);
 
     List<Movie> movie = movieDbResponse.results
         .map(
           (e) => MovieMapper.movieToMoviedb(e),
         )
         .toList();
-
     return movie;
+  }
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get(
+      '/movie/now_playing',
+      queryParameters: {
+        'page': page,
+      },
+    );
+
+    return _jsonToMovies(response.data);
   }
 }
