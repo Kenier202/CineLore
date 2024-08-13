3700acf52e0bbd866e722e6f744d8713a8b82f9f
kenier <kenier202@gmail.com>
2024-08-13 15:23:15 -0500

    feat(search delegate): add new builsSuggestions with images and overview

--- lib/presentation/delegate/search_movie_delegate.dart
+++ lib/presentation/delegate/search_movie_delegate.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:try80/config/helpers/humarn_formats.dart';
+
 import 'package:try80/domain/entities/movie.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
@@ -65,6 +68,97 @@ class SearchMovieDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text("buildSuggestions");
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return _MovieItem(
+              movie: movie,
+              onMovieSelected: close,
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  final Function onMovieSelected;
+  const _MovieItem({
+    required this.movie,
+    required this.onMovieSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final texStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: GestureDetector(
+        onTap: () {
+          onMovieSelected(context, movie);
+        },
+        child: Row(
+          children: [
+            //image
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
+              ),
+            ),
+
+            //text
+            SizedBox(
+              width: size.width * 0.7,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: texStyles.titleMedium,
+                    ),
+                    (movie.overview.length > 100)
+                        ? Text('${movie.overview.substring(0, 100)}...')
+                        : Text(movie.overview),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_half_rounded,
+                          color: Colors.yellow.shade800,
+                        ),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        Text(
+                          HumanFormats.numbers(movie.voteAverage, 1),
+                          style: texStyles.bodyMedium!
+                              .copyWith(color: Colors.yellow.shade900),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
