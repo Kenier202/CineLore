1fc08bfce144eafd6e808c0214d8d9d8e1f73e3f
kenier <kenier202@gmail.com>
2024-08-13 15:21:54 -0500

    chore(movie mapper): add new image to no posterpath

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,7 +16,7 @@ class MovieMapper {
         popularity: moviedb.popularity,
         posterPath: (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-            : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+            : 'https://ih1.redbubble.net/image.5055723050.4010/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
         releaseDate: moviedb.releaseDate,
         title: moviedb.title,
         video: moviedb.video,
