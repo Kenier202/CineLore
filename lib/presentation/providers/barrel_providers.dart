ec77f433ecc5173d0add071592e48417ef6b7a59
kenier <kenier202@gmail.com>
2024-08-21 15:18:44 -0500

    feat(searchMovieProvider): add search provider

--- lib/presentation/providers/barrel_providers.dart
+++ lib/presentation/providers/barrel_providers.dart
@@ -4,3 +4,4 @@ export 'package:try80/presentation/providers/moviedb/movies_provider.dart';
 export 'package:try80/presentation/providers/moviedb/movies_repository_provider.dart';
 export 'package:try80/presentation/providers/moviedb/movies_slideshow_provider.dart';
 export 'package:try80/presentation/providers/moviedb/movie_info_provider.dart';
+export 'package:try80/presentation/providers/search/search_movies_provider.dart';
